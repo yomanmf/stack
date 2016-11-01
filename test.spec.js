496839db22bdc0dc29dd73a1e379370730376c1e
Dmitry Sorin <info@staypositive.ru>
2016-11-01 11:15:48 +0300

    Update test.spec.js

--- test.spec.js
+++ test.spec.js
@@ -13,4 +13,12 @@ describe('Stack testsuite', function () {
         assert.strictEqual(typeof stack.peek, 'function', 'stack.peek is not a function');
         assert.strictEqual(typeof stack.isEmpty, 'function', 'stack.isEmpty is not a function');
     });
+    
+    it('should add items on top of stack with push()', function () {
+        const stack = new Stack;
+
+        assert.doesNotThrow(function () {
+            stack.push(1);
+        }, 'stack.push() throws instead of adding item on top of stack');
+    });
 });
