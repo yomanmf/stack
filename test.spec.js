7b175e48e8847270e9cce27ff5f7c06e71245fcf
Dmitry Sorin <info@staypositive.ru>
2016-11-01 11:12:24 +0300

    Update test.spec.js

--- test.spec.js
+++ test.spec.js
@@ -6,5 +6,11 @@ const Stack = require('./index.js');
 describe('Stack testsuite', function () {
     it('should use expected API', function() {
         assert.strictEqual(typeof Stack, 'function', 'Stack is not a function');
+        
+        const stack = new Stack;
+        assert.strictEqual(typeof stack.push, 'function', 'stack.push is not a function');
+        assert.strictEqual(typeof stack.pop, 'function', 'stack.pop is not a function');
+        assert.strictEqual(typeof stack.peek, 'function', 'stack.peek is not a function');
+        assert.strictEqual(typeof stack.isEmpty, 'function', 'stack.isEmpty is not a function');
     });
 });
