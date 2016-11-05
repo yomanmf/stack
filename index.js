5020a89403d5b865bc3abf10de4b9a4d91a6fae6
Alexander <sanyatest1@yandex.ru>
2016-11-06 02:22:15 +0300

    add jsDoc to every function

--- index.js
+++ index.js
@@ -5,22 +5,50 @@ const assert = require('assert');
 class Stack {
 
 /**
- * @constructor Stack
- * @summary Creates an empty array
+ * Creates an empty array
+ * 
+ * @constructor
+ * @this {Stack}
  */
 
   constructor(stack) {
     this.stack = [];
   }
 
+/**
+ * Adds an element to stack
+ * 
+ * @function
+ * @this {Stack}
+ * @example push(100500);
+ * @param value - value that you wish to put in stack
+ */
+
+
   push(value) {
     this.stack.push(value);
   }
+
+/**
+ * Removes an element from stack
+ * 
+ * @function
+ * @this {Stack}
+ * @example pop();
+ */
 
   pop() {
     this.isEmpty;
       return this.stack.shift;
   }
+
+/**
+ * Returns the first element in stack
+ * 
+ * @function
+ * @this {Stack}
+ * @example peek();
+ */
 
   peek() {
     if (!this.stack.length) {
@@ -30,6 +58,15 @@ class Stack {
       return this.stack[0];
     }
   }
+
+/**
+ * Checks if stack is empty
+ * 
+ * @function
+ * @this {Stack}
+ * @returns true/false
+ * example stack.isEmpty();
+ */
 
   isEmpty() {
     assert(this.stack[0], 'The stack is empty');
